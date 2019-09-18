d1f01afb08e0ec936d6daa4daa61587af26b21e6
Park Mael IL <bitcocom@empas.com>
2019-09-18 12:08:59 +0900

    commit

---
+++ JavaTPC/src/TPC31.java
@@ -0,0 +1,18 @@
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+	public static void main(String[] args) {
+      //Inflearn inf=new Inflearn();      
+      //inf.tpc();
+      Inflearn.tpc();
+      //inf.java();
+      Inflearn.java();
+      
+      // Java API 생성자 private
+      //System sys=new System();
+      System.out.println("출력");
+      //Math m=new Math();
+      int v=Math.max(10, 20);
+      System.out.println(v);
+	}
+}
---
+++ JavaTPC/src/kr/tpc/Inflearn.java
@@ -0,0 +1,15 @@
+package kr.tpc;
+
+public class Inflearn {
+   private Inflearn() {
+	   
+   }
+   //  인스턴스메서드	
+   public static void tpc() {
+	   System.out.println("TPC강의 너무 재미있다.");
+   }
+   //  클래스메서드
+   public static void java() {
+	   System.out.println("Java강의 너무 재미있다.");
+   }
+}
